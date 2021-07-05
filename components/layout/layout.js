b7cbfde5eb4eff7ff94da02efca316a9561c5180
Amit Rajq <amitraj.2383@gmail.com>
2021-07-05 14:59:29 +0100

    Revert: Removed ./ from layour.js

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from 'Header';
-import Footer from 'Footer';
+import Header from './Header';
+import Footer from './Footer';
 import Head from "next/head";
 
 export default class Layout extends Component {
