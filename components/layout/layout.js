10e4fc63195a0b72bc4d57323b3459579e3fd600
Amit Rajq <amitraj.2383@gmail.com>
2021-07-05 17:00:10 +0100

    footer and header with small letters

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './header';
+import Footer from './footer';
 import Head from "next/head";
 
 export default class Layout extends Component {
