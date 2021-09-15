48b777d1d0030dd195c322c23683480d8c821291
Amit Rajq <amitraj.2383@gmail.com>
2021-09-15 22:40:30 +0100

    Next Image with lazy loading

--- components/articles/recent-article.js
+++ components/articles/recent-article.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Image from 'next/image'
 
 export default class RecentArticle extends Component {
     constructor(props) {
@@ -8,11 +9,16 @@ export default class RecentArticle extends Component {
         return (
             <div className="article recent">
                 <div className="image-wrapper">
-                    <picture>
-                        <source srcSet={this.props.articleData.imageUrl+'?webp'} type='image/webp'  style={{width:'100%', height:'100%'}}/>
-                        <source srcSet={this.props.articleData.imageUrl} type='image/jpeg'  style={{width:'100%', height:'100%'}}/>
-                        <img src={this.props.articleData.imageUrl}  style={{width:'100%', height:'100%'}}/>
-                    </picture>
+                <Image
+                    src={this.props.articleData.imageUrl} 
+                    alt="Car comparison blog" 
+                    width='100%' 
+                    height='100%' 
+                    loading="lazy"
+                    layout="responsive"
+                    quality="75"
+                    placeholder="empty"
+                    /> 
                     <span className="article-date fs-8 fw-bold text-white">{this.props.articleData.date}</span>
                 </div>
                 <div className="artical-content p-2"> 
--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 // next.config.js
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const nextImageConfig = {
+    images: {
+        domains: ['suggestrank.com'], // your domain
+    },
+};
 
 module.exports = withPlugins([
   [optimizedImages, {
     /* config for next-optimized-images */
   }],
-
+ 
+  
   // your other plugins here
 
-]);
+],
+nextImageConfig);
