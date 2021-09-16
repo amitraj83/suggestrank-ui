7f2bde622b582742546a70af9a5681f5b2345672
Amit Rajq <amitraj.2383@gmail.com>
2021-09-16 22:46:58 +0100

    gtag defer

--- pages/_document.js
+++ pages/_document.js
@@ -19,8 +19,8 @@ class MyDocument extends Document {
           />
        
           
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-7D68J6S46J"></script>
-          <script dangerouslySetInnerHTML={{__html:`
+          <script async defer src="https://www.googletagmanager.com/gtag/js?id=G-7D68J6S46J"></script>
+          <script async defer dangerouslySetInnerHTML={{__html:`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments)}
             gtag('js', new Date());
